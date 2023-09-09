0ce925ac1fe2b16bb0b0699054253a98e1e20f4a
Mtrilokesh <113591465+Mtrilokesh@users.noreply.github.com>
2023-09-09 16:53:53 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,127 @@
+export const projects = [
+  {
+    title: "Snake Game",
+    subtitle: "html_css_js",
+    description:
+      "Create a simple snake game which almost played by everyone, by using HTML, CSS, and javascript.",
+    image: "./project-1.gif",
+    link: "http://127.0.0.1:5501/index.html",
+  },
+  {
+    title: "From validation",
+    subtitle: "html_css_js",
+    description: "Form validation using html css and javascript.",
+    image: "./project-2.gif",
+    link: "http://127.0.0.1:5500/form.html",
+  },
+  {
+    title: "Calculator",
+    subtitle: "React JS",
+    description: "Calculator using React JS",
+    image: "./project-3.gif",
+    link: "http://127.0.0.1:5502/index.html",
+  },
+  {
+    title: "  countdown-timer",
+    subtitle: "React JS",
+    description: "countdown-timer using React JS",
+    image: "./project-4.gif",
+    link: "http://localhost:3000/",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const certification = [
+  {
+    name: "Python(Basic)",
+    company: "HackerRank",
+    image: "./img1.png",
+    link: "https://www.hackerrank.com/certificates/aaaed28b4e7e",
+  },
+  {
+    name: "Introduction to Web Development with HTML, CSS, JavaScript",
+    company: "IBM",
+    image: "./img2.png",
+    link: "https://www.coursera.org/account/accomplishments/certificate/GX5F6TXUWGAS",
+  },
+  {
+    name: "Learning Full Stack React",
+    company: "Infosys Springboard",
+    image: "./img3.png",
+    link: "https://drive.google.com/file/d/1TY-HGaILmpVwxEUfix0Vq5qvzLsSIJR3/view?usp=sharing",
+  },
+  {
+    name: " FSD Front-end Development: Testing Tools",
+    company: "Infosys Springboard",
+    image: "./img4.png",
+    link: "https://drive.google.com/file/d/1_0sqa4_T8vLjF2nftLvi7YpPB2j1_Klf/view?usp=sharing",
+  },
+  {
+    name: "Introduction to Programming Using Python",
+    company: "Microsoft",
+    image: "./img5.png",
+    link: "https://www.certiport.com/portal/Pages/ViewTranscript.aspx?printview=true&defaultlang=ENU",
+  },
+  {
+    name: "Git &amp; GitHub: Working with Git Repositories",
+    company: "Infosys Springboard",
+    image: "./img6.png",
+    link: "https://drive.google.com/file/d/1RGod_wcq4-kFmukjnf3GR90JlA0sppWy/view?usp=sharing",
+  },
+  {
+    name: "MySQL",
+    company: "Linkedin Learning ",
+    image: "./img7.png",
+    link: "https://www.linkedin.com/learning/certificates/14278af313ff031eb7b618baa192aab8cc5fc554514d92018fd85e816a49b5c0",
+  },
+  {
+    name: "PHP Essentials",
+    company: "Linkedin Learning",
+    image: "./img8.png",
+    link: "https://www.linkedin.com/learning/certificates/42a1541ad60a25ccf977ebdbd8c25957137d45b6495385cd2e2201c70e96dbb0",
+  },
+];
+
+export const skills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Jest",
+  "Python",
+  "MySQL",
+  "Thailwind CSS",
+  "Git and Github",
+  "Webpack",
+];
